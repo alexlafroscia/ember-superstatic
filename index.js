063a7e8011ffd921b687051e49cc11a75d556b2c
Alex LaFroscia <alex@lafroscia.com>
2015-10-28 12:36:21 -0400

    Add ability to import Superstatic config

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 /* jshint node: true */
 'use strict';
 
+var superstatic = require('superstatic');
+
 module.exports = {
-  name: 'ember-superstatic'
+  name: 'ember-superstatic',
+
+  serverMiddleware: function(config) {
+    config.app.use(superstatic());
+  },
+
+  contentFor: function(type) {
+    if (type === 'head') {
+      return "<script src='/__/env.js'></script>";
+    }
+  }
 };
---
+++ tests/dummy/app/controllers/application.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+import env from 'ember-superstatic/environment';
+
+const { Controller } = Ember;
+
+export default Controller.extend({
+
+  env: env
+
+});
