aa89751e0b447c534caf421d911dbff2ee3b0088
Alex LaFroscia <alex@lafroscia.com>
2015-10-28 17:15:27 -0400

    Add keys to the environment in production
    Any key in the environment that is prefixed will be provided to the Ember app.  For example, an environment variable like:
    
    ```bash
    SUPERSTATIC_GREETING='hello, world!'`
    ```
    
    can be accessed inside Ember as:
    
    ```javascript
    import env from 'superstatic/environment';
    console.log(env.GREETING); // -> "hello, world!"
    ```

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,10 +1,13 @@
 import Ember from 'ember';
 import env from 'superstatic/environment';
 
-const { Controller } = Ember;
+const { Controller, computed } = Ember;
+const { alias } = computed;
 
 export default Controller.extend({
 
-  env: env
+  env: env,
+
+  greeting: alias('env.GREETING')
 
 });
